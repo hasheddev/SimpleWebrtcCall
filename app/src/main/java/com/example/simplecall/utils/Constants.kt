c78f0eeb4a89ba775227b4e41b447b092bd23159
Adebayo Shola <sholaadedev@gmail.com>
2025-06-28 17:30:55 +0100

    modify constants

--- app/src/main/java/com/example/simplecall/utils/Constants.kt
+++ app/src/main/java/com/example/simplecall/utils/Constants.kt
@@ -3,5 +3,4 @@ package com.example.simplecall.utils
 object Constants {
     const val TIME_OUT_DURATION_MS = 20000L
      fun getWebSocketUrl(username:String) = "ws://10.0.2.2:8080/?username=$username"
-    //fun getWebSocketUrl(username:String) = "ws://192.168.16.100:3007/?username=$username"
 }
